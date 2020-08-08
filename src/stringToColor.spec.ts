569e6f188e6a5972bd4c107344d02152b8c186f4
David Calvo Meier <davidcalvomeier@protonmail.com>
2020-08-08 17:00:42 +0200

    added tests

---
+++ src/stringToColor.spec.ts
@@ -0,0 +1,55 @@
+import { LimitColor, stringToColor } from "./stringToColor";
+
+test('stringToColor in hex', () => {
+  const basicColorHex = stringToColor('test', false)
+  expect(basicColorHex).toMatchSnapshot()
+})
+
+test('stringToColor in RGB',() =>{
+  const basicColorRGB = stringToColor('test', true)
+  expect(basicColorRGB).toMatchSnapshot()
+})
+
+test('test limits min functions', () => {
+  const limits: LimitColor = {
+    blue: { min: -1, max: 255},
+    green: { min: -1, max: 255},
+    red: { min: -1, max: 255},
+  }
+  expect(() => {
+    stringToColor('test', false, limits)
+  }).toThrow('The limits must be between 0 and 255 both included')
+})
+
+test( 'limits max function', () => {
+  const limits: LimitColor = {
+    blue: { min: 0, max: 256},
+    green: { min: 0, max: 255},
+    red: { min: 0, max: 255},
+  }
+  expect(() => {
+    stringToColor('test', false, limits)
+  }).toThrowError('The limits must be between 0 and 255 both included')
+})
+
+test('limits: the minimum cannot be greater than the maximum', () => {
+  const limits: LimitColor = {
+    blue: { min: 3, max: 2},
+    green: { min: 0, max: 255},
+    red: { min: 0, max: 255},
+  }
+  expect(() => {
+    stringToColor('test', false, limits)
+  }).toThrowError('the minimum cannot be greater than the maximum')
+})
+
+test('check limits', () => {
+  const limits: LimitColor = {
+    blue: { min: 0, max: 2},
+    green: { min: 0, max: 2},
+    red: { min: 0, max: 2},
+  }
+  expect(() => {
+    stringToColor('test', false, limits)
+  }).toMatchSnapshot()
+})
